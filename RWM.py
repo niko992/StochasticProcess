778e9089e4323f26a1f8af823be286030392df0e
niko992 <niko.m92@gmail.com>
2018-12-13 18:39:25 +0100

    return is a Time Series of a function

--- RWM.py
+++ RWM.py
@@ -22,80 +22,78 @@ def Cmat(P):
 
 
 # Random Walk Metropolis
-def RWM(s2, P):
+def RWM(s2, P, f):
     M = np.ceil(P / 2)
     C = Cmat(P)
     zero = np.zeros((P,))
-    Csi = np.zeros((P, cts.N))
-    Csi[:, 0] = np.random.multivariate_normal(zero, C)
-
+    #Csi = np.zeros((P, cts.N))
+    Csi_old = np.random.multivariate_normal(zero, C)
+    q=np.zeros((cts.N,))
+    q[0]=f(Csi_old)
     for i in range(1, cts.N):
         eps = np.random.multivariate_normal(zero, s2 * C)
-        Z = Csi[:, i - 1] + eps
+        Z = Csi_old + eps
         U = np.random.uniform(0, 1)
-        a = np.min([fct.f(Z, M) / fct.f(Csi[:, i - 1], M), 1])
+        a = np.min([fct.f(Z, M) / fct.f(Csi_old, M), 1])
         if U < a:
-            Csi[:, i] = Z
-        else:
-            Csi[:, i] = Csi[:, i - 1]
-    return Csi
+            Csi_old = Z
+        q[i]=f(Csi_old)
+    return q
 
 
-def RWM2(s2, P):
+def RWM2(s2, P, f):
     M = np.ceil(P / 2)
     C = Cmat(P)
     zero = np.zeros((P,))
-    Csi = np.zeros((P, cts.N))
-    Csi[:, 0] = np.random.multivariate_normal(zero, C)
+    Csi_old = np.random.multivariate_normal(zero, C)
+    q = np.zeros((cts.N,))
+    q[0]=f(Csi_old)
     for i in range(1, cts.N):
         eps = np.random.multivariate_normal(zero, s2 * C)
-        Z = np.sqrt(1 - s2) * Csi[:, i - 1] + eps
+        Z = np.sqrt(1 - s2) * Csi_old + eps
         U = np.random.uniform(0, 1)
-        if U < np.min([fct.f(Z, M) / fct.f(Csi[:, i - 1], M), 1]):
-            Csi[:, i] = Z
-        else:
-            Csi[:, i] = Csi[:, i - 1]
-    return Csi
+        if U < np.min([fct.f(Z, M) / fct.f(Csi_old, M), 1]):
+            Csi_old = Z
+        q[i]=f(Csi_old)
+    return q
 
 
-def RWM3(s2, P):
+def RWM3(s2, P, f):
     M = np.ceil(P / 2)
     C = Cmat(P)
     zero = np.zeros((P,))
-    Csi = np.zeros((P, cts.N))
+    q = np.zeros((cts.N,))
     alpha = 0.000001
     ID = np.diag(np.ones(P))
     dictionary = sp.optimize.minimize(
         fct.minus_log_posterior,  np.random.multivariate_normal(zero, C), args=(M), method='BFGS')
-    pdb.set_trace()
     csi_map = dictionary['x']
-    Csi[:, 0] = csi_map
-    print(csi_map)
+    Csi_old = csi_map
+    q[0]=f(Csi_old)
     H = dictionary['hess_inv'] + alpha*ID
     for i in range(1, cts.N):
         eps = np.random.multivariate_normal(zero, H)
         Z = csi_map + eps
         U = np.random.uniform(0, 1)
-        if U < np.min([fct.f(Z, M) / fct.f(Csi[:, i - 1], M), 1]):
-            Csi[:, i] = Z
-        else:
-            Csi[:, i] = Csi[:, i - 1]
-    return Csi
+        if U < np.min([fct.f(Z, M) / fct.f(Csi_old, M), 1]):
+            Csi_old = Z
+        q[i]=f(Csi_old)
+    return q
 
 
-def RWM4(s2, P):
+def RWM4(s2, P, f):
     M = np.ceil(P / 2)
     C = Cmat(P)
     IP = np.diag(np.ones(P))
     Is2 = IP*s2
     zero = np.zeros((P,))
-    Csi = np.zeros((P, cts.N))
+    q = np.zeros((cts.N, ))
 
     res = sp.optimize.minimize(fct.minus_log_posterior, np.random.multivariate_normal(zero, C), args=(M), method='BFGS')
     csi_map = res['x']
 
-    Csi[:, 0] = csi_map
-
+    Csi_old = csi_map
+    q[0]=f(Csi_old)
     def G(x):
         return fct.G(x, M)
 
@@ -110,10 +108,9 @@ def RWM4(s2, P):
     A_gamma = np.dot(sqrt_C, np.dot(sp.linalg.sqrtm(IP-Is2+np.linalg.inv(IP+H_gamma)), sqrt_C))
     for i in range(1, cts.N):
         eps = np.random.multivariate_normal(zero, s2*C_gamma)
-        Z = np.dot(A_gamma, Csi[:, i - 1]) + eps
+        Z = np.dot(A_gamma, Csi_old) + eps
         U = np.random.uniform(0, 1)
-        if U < np.min([fct.f(Z, M) / fct.f(Csi[:, i - 1], M), 1]):
-            Csi[:, i] = Z
-        else:
-            Csi[:, i] = Csi[:, i - 1]
-    return Csi
+        if U < np.min([fct.f(Z, M) / fct.f(Csi_old, M), 1]):
+            Csi_old = Z
+        q[i]=f(Csi_old)
+    return q
--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import RWM as RWM
-import matplotlib
-matplotlib.use('tkAgg')
+#import matplotlib
+#matplotlib.use('tkAgg')
 import matplotlib.pyplot as plt
-
+import numpy as np
+import scipy as sp
+import functions as fct
+def intexp(csi):
+    x=np.linspace(0,1,100)
+    return sp.integrate.trapz(fct.u(x,csi),x)
 
 def main():
+#    s2 = 0.5
+#    P = 10
+#    chain = RWM.RWM(s2, P)
+#    print(chain.shape)
+#    plt.plot(chain[0, :])
+#    plt.show()
+
     s2 = 0.5
-    P = 10
-    '''chain = RWM.RWM(s2, P)
+    P=10
+    chain = RWM.RWM2(s2, P, intexp)
     print(chain.shape)
-    plt.plot(chain[0, :])
+    plt.plot(chain)
     plt.show()
+    print(np.mean(chain))
 
-    chain = RWM.RWM2(s2, P)
-    print(chain.shape)
-    plt.plot(chain[0, :])
-    plt.show()'''
-
-    chain = RWM.RWM4(s2, P)
-    print(chain.shape)
-    plt.plot(chain[0, :])
-    plt.show()
+#    chain = RWM.RWM4(s2, P)
+#    print(chain.shape)
+#    plt.plot(chain[0, :])
+#    plt.show()
 
 
 if __name__ == "__main__":
